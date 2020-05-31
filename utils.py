a1496498120348c7faba77f1d0f277ddd3b856fd
Nick Kullman <Nick.Kullman@gmail.com>
2020-05-31 15:43:48 -0700

    Add ability to save/view pixel arrays

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 
 def rbg_to_gray(rgb):
@@ -10,3 +11,19 @@ def dist(p1, p2, lnorm=1):
     Defaults to lnorm=1 (Manhattan distance).
     """
     return np.linalg.norm(np.array(p1)-np.array(p2), lnorm)
+
+def make_image(pixel_array, filename=None):
+    """Generate the image from the pixel array
+
+    Args:
+        pixel_array (np.array): pixel array to make into an image
+        filename (str, optional): Where to save the image file. Defaults to None, which just shows the plot and does not save it.
+    """
+    
+    plt.imshow(pixel_array, interpolation='nearest')
+    plt.axis('off')
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename, bbox_inches='tight')
+
