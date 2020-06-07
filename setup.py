262b77e72f37b9117df4471fa4678f21e6689ca1
Nick Kullman <Nick.Kullman@gmail.com>
2020-06-06 20:59:11 -0700

    Create a barebones setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='vrpssratari',
+    version='0.0.1',
+    author='Nicholas Kullman',
+    author_email='nicholas.kullman@etu.univ-tours.fr',
+    description='Atari-like playable version of the VRPSSR',
+)
