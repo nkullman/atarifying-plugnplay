258d43f3352098cd716151e3695420613a92c1c5
Nick Kullman <Nick.Kullman@gmail.com>
2020-06-06 20:58:51 -0700

    Better check on rgb array

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def rbg_to_gray(rgb):
     """Convert an RGB image to a grayscale one"""
-    assert len(rgb)==3, "Invalid rgb array passed"
+    assert rgb.shape[0] == 3, "RGB array must be channels-first"
     return 0.2989 * rgb[0] + 0.5870 * rgb[1] + 0.1140 * rgb[2]
 
 def dist(p1, p2, lnorm=1):
