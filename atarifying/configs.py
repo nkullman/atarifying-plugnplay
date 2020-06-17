797b62ecd85a875cce35052dfd83f57262ac7f39
Nick Kullman <Nick.Kullman@gmail.com>
2020-06-16 21:34:58 -0700

    Add notes about potential config revisions
    Currently just dumped in the tuned config files from Ray
    from their work in the Atari env. Tuning these params will
    likely lead to improvements.

--- atarifying/configs.py
+++ atarifying/configs.py
@@ -1,3 +1,6 @@
+
+# TODO do we want to add periodic re-evaluation by default?
+
 APEX_CONFIG = {
     
     # Using pretty much the default tuned config for APE-X Atari
@@ -30,6 +33,18 @@ APEX_CONFIG = {
     "target_network_update_freq": 50000,
     "timesteps_per_iteration": 25000,
 
+    # Params to consider tweaking:
+    #     items noted in main_dqn (since apex config starts from dqn config), plus... (values below are defaults)
+    #     "n_step": 3,
+    #     "num_gpus": 1,
+    #     "num_workers": 32,
+    #     "buffer_size": 2000000,
+    #     "learning_starts": 50000,
+    #     "train_batch_size": 512,
+    #     "rollout_fragment_length": 50,
+    #     "target_network_update_freq": 500000,
+    #     "timesteps_per_iteration": 25000,
+
 }
 
 DQN_DISTRL_CONFIG = {
@@ -60,6 +75,45 @@ DQN_DISTRL_CONFIG = {
     "prioritized_replay_beta_annealing_timesteps": 2000000,
     "num_gpus": 0.2,
     "timesteps_per_iteration": 10000,
+
+    # Params to consider tweaking:
+    #
+    # "hiddens": [256], # i think we used 512 before? grid search?
+    # "n_step": 1, # gridsearch [1,3]?
+    # "exploration_config": {
+    #         "type": "EpsilonGreedy",
+    #         "initial_epsilon": 1.0,
+    #         "final_epsilon": 0.02,
+    #         "epsilon_timesteps": 10000,
+    # },
+    # "timesteps_per_iteration": 1000, # WHEN YOU CALL .train THIS IS HOW MANY STEPS ITS GOING TO DO -- NEED TO BUMP THIS UP
+    # "target_network_update_freq": 500,
+    # # === Replay buffer ===
+    # "buffer_size": 50000,
+    # "prioritized_replay_alpha": 0.6,# Alpha parameter for prioritized replay buffer.
+    # "prioritized_replay_beta": 0.4,# Beta parameter for sampling from prioritized replay buffer.
+    # "final_prioritized_replay_beta": 0.4,# Final value of beta (by default, we use constant beta=0.4).
+    # "prioritized_replay_beta_annealing_timesteps": 20000,
+    # "model": {
+    #         # === For details, see https://github.com/ray-project/ray/blob/master/rllib/models/catalog.py === #
+    #         # "conv_filters": None,   # Filter config. List of [out_channels, kernel, stride] for each filter
+    #         # "conv_activation": "relu",  # Nonlinearity for built-in convnet
+    #         # "fcnet_activation": "tanh", # Nonlinearity for fully connected net (tanh, relu)
+    #         # "fcnet_hiddens": [256, 256],    # Number of hidden layers for fully connected net
+    #         # Interesteing, can toggle LSTM (options are described at above link):
+    #         # "use_lstm": False,  # Whether to wrap the model with an LSTM.
+    #     },
+    # "optimizer": {
+    #     # varies by optimizer... options?
+    # },
+    # "lr": 5e-4,   # The default learning rate.
+    # "gamma": 0.99,  # Discount factor of the MDP.
+    # "adam_epsilon": 1e-8,# Adam epsilon hyper parameter
+    # "grad_clip": 40,# If not None, clip gradients during optimization at this value
+    # "learning_starts": 1000,# How many steps of the model to sample before learning starts.
+    # "rollout_fragment_length": 4,# Update the replay buffer with this many samples at once.
+    # "train_batch_size": 32,# Size of a batch sampled from replay buffer for training.
+
 }
 
 DQN_RAINBOW_CONFIG = {
@@ -93,6 +147,44 @@ DQN_RAINBOW_CONFIG = {
         "dim": 42
     },
     "timesteps_per_iteration": 1000,                            # NOT DIFF this is default val; adding here for use in computing num iters
+
+    # Params to consider tweaking:
+    #
+    # "hiddens": [256], # i think we used 512 before? grid search?
+    # "n_step": 1, # gridsearch [1,3]?
+    # "exploration_config": {
+    #         "type": "EpsilonGreedy",
+    #         "initial_epsilon": 1.0,
+    #         "final_epsilon": 0.02,
+    #         "epsilon_timesteps": 10000,
+    # },
+    # "timesteps_per_iteration": 1000, # WHEN YOU CALL .train THIS IS HOW MANY STEPS ITS GOING TO DO -- NEED TO BUMP THIS UP
+    # "target_network_update_freq": 500,
+    # # === Replay buffer ===
+    # "buffer_size": 50000,
+    # "prioritized_replay_alpha": 0.6,# Alpha parameter for prioritized replay buffer.
+    # "prioritized_replay_beta": 0.4,# Beta parameter for sampling from prioritized replay buffer.
+    # "final_prioritized_replay_beta": 0.4,# Final value of beta (by default, we use constant beta=0.4).
+    # "prioritized_replay_beta_annealing_timesteps": 20000,
+    # "model": {
+    #         # === For details, see https://github.com/ray-project/ray/blob/master/rllib/models/catalog.py === #
+    #         # "conv_filters": None,   # Filter config. List of [out_channels, kernel, stride] for each filter
+    #         # "conv_activation": "relu",  # Nonlinearity for built-in convnet
+    #         # "fcnet_activation": "tanh", # Nonlinearity for fully connected net (tanh, relu)
+    #         # "fcnet_hiddens": [256, 256],    # Number of hidden layers for fully connected net
+    #         # Interesteing, can toggle LSTM (options are described at above link):
+    #         # "use_lstm": False,  # Whether to wrap the model with an LSTM.
+    #     },
+    # "optimizer": {
+    #     # varies by optimizer... options?
+    # },
+    # "lr": 5e-4,   # The default learning rate.
+    # "gamma": 0.99,  # Discount factor of the MDP.
+    # "adam_epsilon": 1e-8,# Adam epsilon hyper parameter
+    # "grad_clip": 40,# If not None, clip gradients during optimization at this value
+    # "learning_starts": 1000,# How many steps of the model to sample before learning starts.
+    # "rollout_fragment_length": 4,# Update the replay buffer with this many samples at once.
+    # "train_batch_size": 32,# Size of a batch sampled from replay buffer for training.
 }
 
 IMPALA_CONFIG = {
@@ -133,6 +225,18 @@ PPO_CONFIG = {
     "observation_filter": "NoFilter",
     "vf_share_layers": True,
     "num_gpus": 1,
+
+    # Params to consider tweaking:
+    #     # Clip param for the value function. Note that this is sensitive to the
+    #     # scale of the rewards. If your expected V is large, increase this.
+    #     "vf_clip_param": 10.0,
+    #     "rollout_fragment_length": 200,
+    #     # Number of timesteps collected for each SGD round. This defines the size
+    #     # of each SGD epoch.
+    #     "train_batch_size": 4000,
+    #     # Total SGD batch size across all devices for SGD. This defines the
+    #     # minibatch size within each epoch.
+    #     "sgd_minibatch_size": 128,
 
 }
 
